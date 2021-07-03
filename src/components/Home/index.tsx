8d09eb7a4379aba6bd33a916502d8b494d2777a5
TravisTHII <travisthackneyii@gmail.com>
2021-07-02 20:08:18 -0400

    Update: UI & Responsiveness

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,15 +6,17 @@ export const Home = () =>
     <Header>
       <div></div>
     </Header>
-    <div className="home flex_ui">
-      <div className="logo flex_ui">
-        <div className="pokemon_logo">
-          <img src="/images/pokemon.svg" alt="Pokémon logo" />
+    <div id="home" className="flex_ui">
+      <div className="home_container flex_ui">
+        <div className="logo flex_ui">
+          <div className="pokemon_logo">
+            <img src="/images/pokemon.svg" alt="Pokémon logo" />
+          </div>
+          <div className="tag_line">
+            <p>Gotta Search ‘Em All!</p>
+          </div>
         </div>
-        <div className="tag_line">
-          <p>Gotta Search ‘Em All!</p>
-        </div>
+        <Search />
       </div>
-      <Search />
     </div>
   </>
