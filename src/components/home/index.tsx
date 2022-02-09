d53d5b416c38aadaefff515751ab910ccbbf1227
unknown <travisthackneyii@gmail.com>
2022-02-08 22:48:14 -0500

    updated styling

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -6,10 +6,10 @@ export const Home = () => (
     <Header className="home_header" />
     <div id="home" className="flex_ui">
       <div className="home_container flex_ui">
-        <div className="logo flex_ui">
+        <div className="logo_container flex_ui">
           <div className="pokemon_logo">
-            <div className="logo_container">
-              <img src="/images/pokemon.svg" alt="Pokémon logo" />
+            <div className="logo">
+              <img src="/images/pokemon.svg" alt="Pokémon" />
             </div>
           </div>
           <div className="tag_line">
