3a1df4b96d953918f09fe502f7374c0dea0f316b
unknown <travisthackneyii@gmail.com>
2022-02-13 00:11:45 -0500

    created mobile search component

--- src/components/header/mobile/index.tsx
+++ src/components/header/mobile/index.tsx
@@ -4,15 +4,23 @@ import { IconContext } from 'react-icons'
 import { FiMenu, FiSearch } from 'react-icons/fi'
 import { Menu } from './Menu'
 
+const MobileSearch = () => {
+  return <div className="mobile_search"></div>
+}
+
 export const MobileMenu = () => {
   const [open, setOpen] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
 
   return (
     <>
       <div className="mobile_header_menu">
         <IconContext.Provider value={{ size: '1.5rem', className: 'icon' }}>
           <div className="container">
-            <button className="mobile_search_button">
+            <button
+              className="mobile_search_button"
+              onClick={() => setShowSearch(!showSearch)}
+            >
               <FiSearch />
             </button>
             <button className="menu_button" onClick={() => setOpen(!open)}>
@@ -21,6 +29,7 @@ export const MobileMenu = () => {
           </div>
         </IconContext.Provider>
       </div>
+      {showSearch && <MobileSearch />}
       {open && createPortal(<Menu />, document.body)}
     </>
   )
